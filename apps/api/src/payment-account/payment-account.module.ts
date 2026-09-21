9857186c4c6e7de9946bc98754f781b4ad793552
Matheus Carmo <rerrol2508@gmail.com>
2026-09-21 16:28:51 -0300

    feat(payment-account): connect/disconnect MP account, own statement, PIX retry

--- apps/api/src/payment-account/payment-account.module.ts
+++ apps/api/src/payment-account/payment-account.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentAccountController } from './payment-account.controller';
+import { PaymentOAuthController } from './payment-oauth.controller';
 import { PaymentAccountService } from './payment-account.service';
 
 @Module({
-  controllers: [PaymentAccountController],
+  controllers: [PaymentAccountController, PaymentOAuthController],
   providers: [PaymentAccountService],
   exports: [PaymentAccountService],
 })
